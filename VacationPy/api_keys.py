2dd8e9aac1cc445fba4b9382150f35447275980a
unknown <laura.perezolvera@gmail.com>
2020-08-01 21:28:10 -0500

    WeatherPy files

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "d6884cbee47ad956fcec26da675d19e8"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB2gxIxMCkOV2thUxMgXwOhGo0hvmhOEss"
